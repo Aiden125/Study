d4fcc5dc726905000c3eb15b16069d19d9cfbe82
HeeSeok <mungml1995@gmail.com>
2023-01-18 01:58:38 +0900

    230118

---
+++ CPP/lower_upper_bound.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int main() {
+	vector<int> a;
+	for(int i=1; i<=3; i++) {
+		a.push_back(i);
+	}
+	for(int i=5; i<=10; i++) {
+		a.push_back(i);
+	}
+	cout << lower_bound(a.begin(), a.end(), 4) - a.begin() << "\n"; // 3
+	
+	return 0;
+}
