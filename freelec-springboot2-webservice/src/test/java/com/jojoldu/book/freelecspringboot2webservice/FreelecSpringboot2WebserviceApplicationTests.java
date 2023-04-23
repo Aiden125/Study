3661f6e33f226898b36f7cd11e0743c47698bcf1
HeeSeok <mungml1995@gmail.com>
2023-04-23 20:17:39 +0900

    230423

--- freelec-springboot2-webservice/src/test/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplicationTests.java
+++ freelec-springboot2-webservice/src/test/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplicationTests.java
@@ -1,7 +1,8 @@
 package com.jojoldu.book.freelecspringboot2webservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class FreelecSpringboot2WebserviceApplicationTests {
