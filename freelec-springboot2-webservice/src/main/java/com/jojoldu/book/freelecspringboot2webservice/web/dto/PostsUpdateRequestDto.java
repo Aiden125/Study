20415f7d3d33a1d8ac745684dae64185447b68ec
HeeSeok <mungml1995@gmail.com>
2023-04-26 00:34:49 +0900

    230426

---
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/freelecspringboot2webservice/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package com.jojoldu.book.freelecspringboot2webservice.web.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
