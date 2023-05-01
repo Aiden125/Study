39eec896385bde73b04851cdb71b57a0486bc114
HeeSeok <mungml1995@gmail.com>
2023-05-01 16:37:19 +0900

    posts delete

--- freelec-springboot2-webservice/src/main/java/com/jojoldu/book/freelecspringboot2webservice/web/IndexController.java
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/freelecspringboot2webservice/web/IndexController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
