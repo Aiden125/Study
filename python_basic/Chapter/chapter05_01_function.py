8f8d26d5e022f5f3d6c645d73ea0509901dc8eab
DESKTOP-KVGJO43\mungm <mungml1995@gmail.com>
2023-12-22 15:05:47 +0900

    231222

--- python_basic/Chapter/chapter05_01_function.py
+++ python_basic/Chapter/chapter05_01_function.py
@@ -37,3 +37,55 @@ d = function_mul3(40)
 print(type(d), d, d.get('v2'), d.items(), d.keys())
 
 
+# 중요파트
+# 인자 갯수에 상관없이 코딩하는 언팩킹에 대해 이해해야 한다.
+
+## *args, **kwars
+## *args(언팩킹) : 가변인자를 사용할 수 있게해준다.
+## 주로 튜플을 사용할 때 사용
+def args_func(*args):
+    for i, v in enumerate(args):
+        print('Result : {}'.format(i), v)
+    print('----')
+
+args_func('Lee')
+args_func('Lee', 'Park')
+args_func('Lee', 'Park', 'Kim')
+
+
+## **kwargs(언팩킹) : 가변인자를 사용할 수 있게해준다.
+## 딕셔너리 형태로 넣김
+def kwargs_func(**kwargs):
+    for v in kwargs.keys():
+        print("{}".format(v), kwargs[v])
+    print('------')
+
+kwargs_func(name1='Lee')
+kwargs_func(name1='Lee', name2='Park')
+kwargs_func(name1='Lee', name2='Park', name3='Cho')
+
+
+# 전체 혼합 예제
+def example(args_1, args_2, *args, **kk):
+    print(args_1, args_2, args, kk)
+
+example(10, 20, 'Lee', 'Kim', 'Park', age1=20, age2=20, age3=40)
+
+print('----------')
+
+# 중첨함수
+def nested_func(num):
+    def func_in_func(num): # 얘는 부모함수안에 있는 함수여서 얘만 호출하는건 불가능하다.
+        print(num)
+    print("In func")
+    func_in_func(num+100)
+
+nested_func(100)
+
+
+
+# 람다식 예제
+# 메모리 절약, 가독성 향상, 코드 간결
+## 함수는 객체 생성 -> 리소스(메모리) 할당
+## 람다는 즉시 실행 함수(Heap 초기화) -> 메모리 초기화
+## 남발 시 가독성 감소
